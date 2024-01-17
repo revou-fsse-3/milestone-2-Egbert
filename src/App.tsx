2a8e1bccf3c477a63479b610010223a0d4f3f66c
Egbert Felica Wibianto <egbert.fewi@gmail.com>
2024-01-17 16:22:00 +0700

    Delete Details

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // App.tsx
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 import WeatherInfo from './componets/WeatherInfo';
 import Loading from './componets/Loading';
